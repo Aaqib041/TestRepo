fa68654a3397f31c3289426c1a281134f76c81ca
unknown <M1023764@A2ML15978.mindtree.com>
2015-09-21 15:53:25 +0530

    initial commit

---
+++ Test/src/com/mindtree/test/TestJava.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mindtree.test;
+
+/**
+ * @author M1023764
+ *
+ */
+public class TestJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Helloooo!!!");
+
+	}
+
+}
